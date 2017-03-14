28f6a8cec5b656da765fd77144d2207c2f64157e
Lydia Katsamberis <lydia@craigslist.org>
2017-03-13 17:00:05 -0700

    clean up generate script a bit

--- generate.js
+++ generate.js
@@ -1,27 +1,27 @@
-var fs = require('fs');
-var path = require('path')
-var glob = require('glob')
-var yaml = require('write-yaml')
+const fs = require('fs');
+const path = require('path')
+const glob = require('glob')
+const yaml = require('write-yaml')
 
-var site_index = '_includes/site-index.html'
+const site_index = '_includes/site-index.html'
 
 glob('gifs/**/*.gif', function (err, files) {
   if (err) {
     throw err
   }
 
-  lists = []
-  gifs = []
+  let lists = []
+  let gifs = []
   files.forEach(function (file) {
-    stat = fs.statSync(file)
+    const stat = fs.statSync(file)
     gifs.push({
       'path': file,
       'modified_time': Math.floor(stat.mtime.getTime() / 1000),
       'extname': path.extname(file)
     })
 
-    trimmed_path = file.replace('gifs/', '')
-    header = trimmed_path.substring(0, trimmed_path.indexOf('/')).replace('-', ' ');
+    const trimmed_path = file.replace('gifs/', '')
+    const header = trimmed_path.substring(0, trimmed_path.indexOf('/')).replace('-', ' ');
     if (typeof lists[header] === 'undefined') {
       lists[header] = []
     }
@@ -31,8 +31,8 @@ glob('gifs/**/*.gif', function (err, files) {
     )
   })
 
-  html = []
-  for (var group in lists) {
+  let html = []
+  for (let group in lists) {
     html.push('<li>' + group + '<ul>' + lists[group].join('') + '</ul></li>');
   }
 
